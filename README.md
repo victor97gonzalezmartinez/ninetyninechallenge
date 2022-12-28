e4c503ad332cf93678cd6387221bdcb5816d7941
Victor Gonzalez <vgonzalez@yopapp.me>
2022-12-28 15:01:22 +0100

    Added improvement comments and remove old sentence on README.md

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ## Before start Springboot
 - Make sure Docker is running.
 - Go to docker-compose.yaml and run services (you will need the Docker plugin)
-- Remove all data from ninetynine collections if is not empty.
 
 ## Local links after starting
 - Mongo Express Interface: http://localhost:8081/db/ninetynine/
--- src/main/kotlin/com/ninetynine/challenge/repository/SharePriceHistoryRepository.kt
+++ src/main/kotlin/com/ninetynine/challenge/repository/SharePriceHistoryRepository.kt
@@ -4,6 +4,11 @@ import com.ninetynine.challenge.model.SharePriceHistory
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface SharePriceHistoryRepository: MongoRepository<SharePriceHistory, String> {
+    // maybe custom queries in prod
+
+    // use projections to avoid showing flag fields
+    // limit results to X amount or remove flag from old registers.
+    // Maybe have different collections for each time series?
     fun findByCompanyIdAndWeeklyIsTrueOrderByDateDesc(companyId: String): List<SharePriceHistory>
     fun findByCompanyIdAndDailyIsTrueOrderByDateDesc(companyId: String): List<SharePriceHistory>
     fun findByCompanyIdAndHourlyIsTrueOrderByDateDesc(companyId: String): List<SharePriceHistory>
